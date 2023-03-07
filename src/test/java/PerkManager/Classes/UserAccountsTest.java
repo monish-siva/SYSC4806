1ff6f7c14ca0412c6d3e5ab11ccd63257b430958
Vijay Ramalingom <vijayramalingom12@gmail.com>
2023-03-07 17:24:10 -0500

    created classes subdirectory for tests and added UserTest to it. Also created UserAccountsTest.

---
+++ src/test/java/PerkManager/Classes/UserAccountsTest.java
@@ -0,0 +1,28 @@
+package PerkManager.Classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserAccountsTest {
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void getUserAccounts() {
+    }
+
+    @Test
+    void getUserByID() {
+    }
+
+    @Test
+    void getID() {
+    }
+
+    @Test
+    void setID() {
+    }
+}
