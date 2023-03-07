0c7a32d1a689ff58b5ee1ae3a0feb72f25aae385
Vijay Ramalingom <vijayramalingom12@gmail.com>
2023-03-07 17:41:00 -0500

    created test for addUser

--- src/test/java/PerkManager/Classes/UserAccountsTest.java
+++ src/test/java/PerkManager/Classes/UserAccountsTest.java
@@ -2,12 +2,26 @@ package PerkManager.Classes;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserAccountsTest {
 
+    //test for addUser method
     @Test
     void addUser() {
+        UserAccounts account = new UserAccounts();
+        User user1 = new User("user1", "password");
+        User user2 = new User("user2", "password");
+        List<User> list = new ArrayList<>();
+        account.addUser(user1);
+        list.add(user1);
+        assertEquals(list, account.getUserAccounts());
+        account.addUser(user2);
+        list.add(user2);
+        assertEquals(list, account.getUserAccounts());
     }
 
     @Test
