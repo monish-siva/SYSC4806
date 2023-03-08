d6f2f625a7131ee08f38e66c09a655dc537743f7
Monishkumar Sivakumar <monish.sivakumar@cmail.carleton.ca>
2023-03-07 23:04:43 -0500

    Revert "Post method created for perk"
    This reverts commit 839bb339d1d1fa24f03c63b23f50043276a61e95.

--- src/main/java/PerkManager/Controllers/PerkRestController.java
+++ src/main/java/PerkManager/Controllers/PerkRestController.java
@@ -1,10 +1,8 @@
 package PerkManager.Controllers;
 
 import PerkManager.Classes.Perk;
-import PerkManager.Classes.PerkList;
 import PerkManager.Classes.User;
 import PerkManager.Classes.UserAccounts;
-import PerkManager.Repositorys.PerkListRepository;
 import PerkManager.Repositorys.PerkRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,27 +14,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PerkRestController {
 
     private PerkRepository perkRepository;
-    private PerkListRepository perkListRepository;
 
     @Autowired
-    public  PerkRestController(PerkRepository perkRepository, PerkListRepository perkListRepository){
+    public  PerkRestController(PerkRepository perkRepository){
         this.perkRepository = perkRepository;
-        this.perkListRepository = perkListRepository;
     }
 
     @RequestMapping(value = "/Perk", method = RequestMethod.GET)
     public Perk getPerk(@RequestParam("perkId") Long perkID) {
         Perk perk = perkRepository.findByID(perkID);
-        return perk;
-    }
-
-    @RequestMapping(value = "/Perk", method = RequestMethod.POST)
-    public Perk addPerk(@RequestParam String location, @RequestParam String card, @RequestParam String membership, @RequestParam String discount) {
-        PerkList perkList = perkListRepository.findByID(1L);
-        Perk perk = new Perk(location, card, membership, discount);
-        perkList.addPerk(perk);
-        perkRepository.save(perk);
-        perkListRepository.save(perkList);
         return perk;
     }
 
