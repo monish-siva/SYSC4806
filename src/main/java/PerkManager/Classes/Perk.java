afee63e8778723c4187b49b7655fcfb34f5f915e
Monishkumar Sivakumar <monish.sivakumar@cmail.carleton.ca>
2023-03-07 23:09:46 -0500

    Revert "Revert "Initialized Perks""
    This reverts commit e27adf747995cca75c7421702ff89aa44104a05d.

--- src/main/java/PerkManager/Classes/Perk.java
+++ src/main/java/PerkManager/Classes/Perk.java
@@ -1,7 +1,19 @@
 package PerkManager.Classes;
 import PerkManager.Classes.PerkSources.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-
+@Entity
+@Table(name = "user_perks")
 public class Perk {
 
+    @Id
+    @GeneratedValue
+    private Long ID;
+
+    public  Perk(){
+
+    }
 }
