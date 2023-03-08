666df5e5a3337a8367d9f9f66e2369f1f83848d7
AayushMallya <aayushmallya72@gmail.com>
2023-03-07 20:14:17 -0500

    new class to contain al;l of the perks

---
+++ src/main/java/PerkManager/Classes/PerkList.java
@@ -0,0 +1,50 @@
+package PerkManager.Classes;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "perk_list")
+public class PerkList {
+
+
+    @OneToMany (cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List <Perk> perks;
+
+    @Id
+    @GeneratedValue
+    private Long ID;
+
+    public PerkList () {
+
+        this.perks = new ArrayList<Perk>();
+    }
+
+    public PerkList(Long id) {
+        this.setID(id);
+        this.perks = new ArrayList<Perk>();
+    }
+
+    public void addPerk(Perk perk) {
+        this.perks.add(perk);
+    }
+
+    public List<Perk> getPerks(){
+        return this.perks;
+    }
+
+    public long getID(){
+        return this.ID;
+    }
+
+    public Perk getPerkByID(Long id){
+        return this.perks.get(Math.toIntExact(id));
+    }
+
+    public void setID(Long id) {
+        this.ID = id;
+    }
+
+}
