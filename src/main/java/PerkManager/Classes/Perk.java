8900bdfabb57d403dcaaf923229d2932edf8737d
Monishkumar Sivakumar <monish.sivakumar@cmail.carleton.ca>
2023-03-07 23:04:57 -0500

    Revert "Created product and related classes"
    This reverts commit cc15ae3876cea69c2445a39904fb48655ef1c554.

--- src/main/java/PerkManager/Classes/Perk.java
+++ src/main/java/PerkManager/Classes/Perk.java
@@ -20,23 +20,21 @@ public class Perk {
 
     private String membership;
 
-    private String discount;
-
     /**
      * Default constructor
      */
     protected Perk(){
+        this.ID = null;
         this.location = "";
         this.card = "";
         this.membership = "";
-        this.discount = "";
     }
 
-    public Perk(String location, String card, String membership, String discount){
+    public Perk(String location, String card, String membership){
+        this();
         this.location = location;
         this.card = card;
         this.membership = membership;
-        this.discount = discount;
     }
 
     public String getLocation(){
@@ -55,10 +53,6 @@ public class Perk {
         return this.ID;
     }
 
-    public String getDiscount() {
-        return this.discount;
-    }
-
     public void setCard(String card) {
         this.card = card;
     }
@@ -73,9 +67,5 @@ public class Perk {
 
     public void setID(Long ID) {
         this.ID = ID;
-    }
-
-    public void setDiscount(String discount) {
-        this.discount = discount;
     }
 }
